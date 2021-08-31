f224469f11a0116592406c88aef5d20087a3782c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-31 10:55:05 +0200

    Added config

---
+++ documentation/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: "LeoCorpLibrary",
+    description: "A C# library with useful features for .NET Applications.",
+    themeConfig: {
+        sidebar: 'auto',
+        sidebarDepth: 3,
+        searchPlaceholder: 'Search...',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Installation', link: '/Install-LeoCorpLibrary' }
+        ],
+        smoothScroll: true,
+        displayAllHeaders: true,
+        repo: 'Leo-Corporation/LeoCorpLibrary',
+        docsRepo: 'Leo-Corporation/LeoCorpLibrary-Web',
+        docsDir: 'documentation',
+        docsBranch: 'main',
+        editLinks: true,
+        editLinkText: 'Help us improve this page!'
+    }
+}
