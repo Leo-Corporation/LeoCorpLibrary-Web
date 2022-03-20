7deea83e335d9fda0375eb4ba8281614f494f2cf
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-03-20 12:10:54 +0100

    Deployed

--- Build.bat
+++ Build.bat
@@ -1,4 +1,2 @@
 @echo off
-vuepress build documentation -d docs && powershell New-Item -Path ./docs -Name "CNAME" -ItemType "file" -Value "leocorplibrary.leocorporation.dev"
-cd documentation
-copy sitemap.xml ..\docs
+vuepress build documentation -d docs && powershell New-Item -Path ./docs -Name "CNAME" -ItemType "file" -Value "leocorplibrary.leocorporation.dev" &&  copy documentation\sitemap.xml docs\sitemap.xml
